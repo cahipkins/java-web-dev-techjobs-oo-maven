869cf6145f8a6a3d8a9be765ddc2f095cda41898
Christina Hipkins <christina_hipkins@comcast.com>
2021-07-07 15:35:42 -0400

    part 4 completed

--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,21 +1,37 @@
 package com.company.incomplete;
 
+
+import com.company.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+
 
 public class JobTests {
     // you can rename the tests, or delete them and rewrite them from scratch, but I went ahead and provided you the names of tests I created -- I followed the writeup pretty honestly
+
+
     @Test
     public void testSettingJobId() {
-
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+        Assertions.assertEquals(1, job1.getId(), .001);
+        Assertions.assertTrue(job2.getId() != job1.getId());
+        Assertions.assertFalse(job2.getId() < job1.getId());
     }
 
     @Test
     public void testJobConstructorSetsAllFields() {
-
+       Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       Assertions.assertTrue(job1 instanceof Job);
     }
 
     @Test
     public void testJobsForEquality() {
+       Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assertions.assertFalse(job1.equals(2));
+        Assertions.assertFalse(job2.equals(1));
 
     }
 
